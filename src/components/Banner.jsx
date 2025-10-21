4e115e94d1a624c6878859832f3984fad52df505
Mattgllrm <matthisgllrm@gmail.com>
2025-10-21 13:21:04 +0200

    fix: hide carousel image counter on mobile

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,13 +1,11 @@
-
-function Banner({ image, title, className }) {
+function Banner({ image, title }) {
   return (
     <section
-      className={`banner ${className}`}
+      className="banner"
       style={{ backgroundImage: `url(${image})` }}
     >
       {title && <h1>{title}</h1>}
     </section>
   );
 }
-
 export default Banner;
